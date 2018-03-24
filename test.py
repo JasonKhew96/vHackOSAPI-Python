1ad2b982ff12a6b99f50b4274a2becea5f713d33
JasonKhew96 <29531167+JasonKhew96@users.noreply.github.com>
2018-03-25 02:43:08 +0800

    Alpha 10 Fix

--- test.py
+++ test.py
@@ -8,4 +8,4 @@ def test_base64():
     assert utils.to_base64('{"lang":"en","uid":"999","accesstoken":"test"}') == 'eyJsYW5nIjoiZW4iLCJ1aWQiOiI5OTkiLCJhY2Nlc3N0b2tlbiI6InRlc3QifQ'
 
 def test_generate_url():
-    assert nw.Network.generate_url(php='test.php', lang='en', uid='999', accesstoken='test') == 'https://api.vhack.cc/mobile/12/test.php?user=eyJsYW5nIjoiZW4iLCJ1aWQiOiI5OTkiLCJhY2Nlc3N0b2tlbiI6InRlc3QifQ&pass=897823e153245f0ce9365268b0822072'
+    assert nw.Network.generate_url(php='test.php', lang='en', uid='999', accesstoken='test') == 'https://api.vhack.cc/mobile/13/test.php?user=eyJsYW5nIjoiZW4iLCJ1aWQiOiI5OTkiLCJhY2Nlc3N0b2tlbiI6InRlc3QifQ&pass=897823e153245f0ce9365268b0822072'
